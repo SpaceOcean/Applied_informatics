02cceaca5cca83b8a95fe283a48b0785cf5c5c8f
SpaceOcean <komsomolenko11@gmail.com>
2020-05-18 02:46:27 +0300

    Первая ревсия первой лабы

---
+++ lab_1/lab_1/main.cpp
@@ -0,0 +1,134 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Влад Комсомоленко on 26.02.2020.
+//  Copyright © 2020 Space. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+
+double funck(double x){
+    double y = x*x*x;
+    return y;
+}
+
+double value_min(std::vector<double> values, int width){
+    double value_min = 100000;
+    for (int i = 0; i < width; i++){
+        if (values[i] < value_min){
+            value_min = values[i];
+        }
+    }
+    return value_min;
+}
+
+double value_max(std::vector<double> values, int width){
+    double value_max = -100000;
+    for (int i = 0; i < width; i++){
+        if (values[i] > value_max){
+            value_max = values[i];
+        }
+    }
+    return value_max;
+}
+
+
+int main(int argc, const char * argv[]) {
+    int columns; int height; double x0; double x1; double value;
+    std::cin >> columns >> height >> x0 >> x1;
+    std::vector<double> values;
+    height++; // для строки с нулём
+    double x_step = (x1 - x0) / columns;
+    for (int i = 0; i < columns; i++){
+        double x = x0 + (i * x_step);
+        values.push_back(funck(x));
+        
+    }
+    double y_min = value_min(values, columns);
+    double y_max = value_max(values, columns);
+    double y_step = (y_max - y_min) / height;
+    
+    // определяем у=0
+    int z_string = 0;
+    double y = values[0];
+    int negative = 0;
+    double difference = values[0];
+    for (int i = 0; i < height; i++){
+        if ((y_max <= 0) && (y_min < 0)){
+            negative = 0;
+            break;
+        }
+        if ((y_min >= 0) && (y_max > 0)){
+            z_string = height - 1;
+            break;
+        }
+        if (values[0] < values[1]){
+            y += y_step;
+            if (abs(y) <= abs(difference)){
+                difference = abs(y);
+                negative = i + 1;
+                z_string = height - negative;
+            }
+        }
+        if (values[0] > values[1]){
+            y -= y_step;
+            if (abs(y) <= abs(difference)){
+                difference = abs(y);
+                z_string = i + 1;
+                negative = height - z_string;
+            }
+        }
+    }
+    
+    //создаём и заполняем пустыми значениями массив
+    const char* ary[height][columns];
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < columns; j++) {
+            ary[i][j] = " ";
+        }
+    }
+    for (int i = 0; i < columns; i++) {
+        
+        ary[z_string][i] = "-";
+    }
+    int steps = 0;
+    //заполняем таблицу графиком
+    for (int i = 0; i < columns; i++){
+        value = abs(values[i]);
+        if (values[i] < 0){
+            while (value > 0) {
+                value -= y_step;
+                steps++;
+            }
+        }
+        if (values[i] >= 0){
+            while (value < y_max) {
+                value += y_step;
+                steps++;
+            }
+        }
+        
+        if (values[i] < 0){
+            steps = steps + z_string;
+        }
+        if (abs(value - y_step) >= value){
+            ary[steps][i] = "#";
+        } else {
+            ary[steps-1][i] = "#";
+        }
+        steps = 0;
+    }
+    for (int i = 0; i < height; i++) {
+        std::cout << i+1 << ")";
+          for (int j = 0; j < columns; j++) {
+              std::cout << ary[i][j];
+          }
+          std::cout << std::endl;
+    }
+    return 0;
+}
